6f3bbb3e15be5835299bca13a8210a40af854a4b
EduardoVeiga <epontesveiga@gmail.com>
2023-03-17 12:36:35 -0300

    Class KronSpeaker created

--- KronForm/Form1.cs
+++ KronForm/Form1.cs
@@ -1,7 +1,8 @@
-using Socket_Cliente;
+//using Socket_Cliente;
 using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using static KronForm.KronSpeaker;
 
 namespace KronForm
 {
@@ -71,68 +72,6 @@ namespace KronForm
             }
 
             return true;
-        }
-
-        public static void StartClient(TextBox _ip, TextBox _port, Label _sendData, string _msg)
-        {
-            byte[] bytes = new byte[1024];
-
-            try
-            {
-                // Connect to a Remote server
-                // Get Host IP Address that is used to establish a connection
-                // If a host has multiple addresses, you will get a list of addresses
-
-                IPHostEntry host = Dns.GetHostEntry("localhost");
-                //IPAddress ipAddress = IPAddress.Parse(_ip.Text);
-                IPAddress ipAddress = host.AddressList[0];
-
-                IPEndPoint remoteEP = new IPEndPoint(ipAddress, Convert.ToInt32(_port.Text));
-
-                // Create a TCP/IP  socket.
-                Socket sender = new Socket(ipAddress.AddressFamily,
-                SocketType.Stream, ProtocolType.Tcp);
-
-                // Connect the socket to the remote endpoint. Catch any errors.
-                try
-                {
-                    // Connect to Remote EndPoint
-                    sender.Connect(remoteEP);
-
-                    _sendData.Text = "Sending data... " + "\n" + _msg;
-
-                    // Encode the data string into a byte array.
-                    byte[] msg = Encoding.ASCII.GetBytes(_msg);
-                    
-                    // Send the data through the socket.
-                    int bytesSent = sender.Send(msg);
-
-                    // Receive the response from the remote device.
-                    int bytesRec = sender.Receive(bytes);
-
-                    // Release the socket.
-                    sender.Shutdown(SocketShutdown.Both);
-                    sender.Close();
-
-                }
-                catch (ArgumentNullException ane)
-                {
-                    MessageBox.Show("ArgumentNullException : {0}", ane.ToString());
-                }
-                catch (SocketException se)
-                {
-                    MessageBox.Show("SocketException : {0}", se.ToString());
-                }
-                catch (Exception e)
-                {
-                    MessageBox.Show("Unexpected exception : {0}", e.ToString());
-                }
-
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.ToString());
-            }
         }
 
         private void btn_confirm_Click(object sender, EventArgs e)
---
+++ KronForm/KronSpeaker.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KronForm
+{
+    internal class KronSpeaker
+    {
+        public static void StartClient(TextBox _ip, TextBox _port, Label _sendData, string _msg)
+        {
+            byte[] bytes = new byte[1024];
+
+            try
+            {
+                // Connect to a Remote server
+                // Get Host IP Address that is used to establish a connection
+                // If a host has multiple addresses, you will get a list of addresses
+
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                //IPAddress ipAddress = IPAddress.Parse(_ip.Text);
+                IPAddress ipAddress = host.AddressList[0];
+
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, Convert.ToInt32(_port.Text));
+
+                // Create a TCP/IP  socket.
+                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.
+                try
+                {
+                    // Connect to Remote EndPoint
+                    sender.Connect(remoteEP);
+
+                    _sendData.Text = "Sending data... " + "\n" + _msg;
+
+                    // Encode the data string into a byte array.
+                    byte[] msg = Encoding.ASCII.GetBytes(_msg);
+
+                    // Send the data through the socket.
+                    int bytesSent = sender.Send(msg);
+
+                    // Receive the response from the remote device.
+                    int bytesRec = sender.Receive(bytes);
+
+                    // Release the socket.
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    MessageBox.Show("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    MessageBox.Show("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+    }
+}
